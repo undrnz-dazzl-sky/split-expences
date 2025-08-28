40bd500fbfbeefc1700fd3133f9762dd049c8019
solomias <miaveryoriginal@gmail.com>
2025-08-28 14:57:45 +0300

    new person can be added using the form now

--- src/App.js
+++ src/App.js
@@ -83,12 +83,34 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
-  return <form className="form-add-friend">
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setName('')
+    setImage("https://i.pravatar.cc/48")
+  }
+
+  return <form className="form-add-friend" onSubmit={handleSubmit}>
     <label>🧑‍🤝‍🧑Friend name</label>
-    <input type="text" />
+    <input type="text" value={name} onChange={e => setName(e.target.value)} />
+
 
     <label>🖼️ Image URL</label>
-    <input type="text" />
+    <input type="text" value={image} onChange={e => setImage(e.target.value)} />
 
     <Button>Add</Button>
   </form>
